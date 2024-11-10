876bd1144ae32e162cb595345d5b60b212dc975a
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2024-11-10 13:23:02 -0800

    Update comments

--- lib/index.ts
+++ lib/index.ts
@@ -7,9 +7,15 @@ export async function complexity(inputFragments: string[], referenceFragments: s
     const inputEmbeddings = await getEmbeddings(inputFragments);
     const referenceEmbeddings = await getEmbeddings(referenceFragments);    
 
-    // rows: input embeddings, columns: reference embeddings
+    // Create a matrix of embedding similarities,
+    // with rows being input embeddings,
+    // and columns being reference embeddings
     const similarityMatrix = pairwiseCalculate(inputEmbeddings, referenceEmbeddings, cosineSimilarity);
+
+    // Calculate softmax of each row to find probability distribution for each input embedding
     const softMaxxed = similarityMatrix.map(softMax);
+
+    // Calculate divergences of each adjacent pair of input embeddings to compute divergence signal
     const divergences = pairwiseReduce(softMaxxed, klDivergence);
 
     return divergences;
